634b8ee8ff2efb41ad7679946b8f159c2cafc4e7
Valto Silva <valtosilva7@gmail.com>
2018-11-05 19:18:49 -0500

    Installed the L5-Swagger library (!! --  NOT WORKING!! --  !!)

--- EB_Hands_Full_Stack/backend/config/app.php
+++ EB_Hands_Full_Stack/backend/config/app.php
@@ -150,6 +150,9 @@ return [
         /*
          * Package Service Providers...
          */
+        \L5Swagger\L5SwaggerServiceProvider::class,
+
+
 
         /*
          * Application Service Providers...
